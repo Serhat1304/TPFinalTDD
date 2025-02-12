37915c6f89c5e97235e1cbdd0569c3cda467ff2d
Serhat1304 <serhatkus01@gmail.com>
2025-02-12 15:32:30 +0100

    Structure pour stocker les solutions

--- src/nqueens.ts
+++ src/nqueens.ts
@@ -2,5 +2,13 @@ export function solveNQueens(n: number) : string[][]{
     if (n === 1) {
         return [["#"]];
     }
+    if (n == 2 || n == 3) {
+        return [];
+    }
     return [];
+
+    const solutions : string [][] = [];
+    const cols = Array(n).fill(-1);
+
+    return solutions
 }
