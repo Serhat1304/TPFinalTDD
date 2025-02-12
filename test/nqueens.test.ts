5bd82209c0779923ebc08840eaee0ef3fd428e66
Serhat1304 <serhatkus01@gmail.com>
2025-02-12 15:06:45 +0100

    Implémentations n=2 et n=3 au vert (0 solutions)

--- test/nqueens.test.ts
+++ test/nqueens.test.ts
@@ -6,5 +6,13 @@ describe('NQueens', () => {
       expect(solutions.length).toBe(1);
       expect(solutions[0]).toEqual(["#"]);
     });
+    it('should return 0 solution for n=2', () => {
+        const solutions = solveNQueens(2);
+        expect(solutions.length).toBe(0);
+    });
+    it('should return 0 solution for n=3', () => {
+        const solutions = solveNQueens(3);
+        expect(solutions.length).toBe(0);
+    });
   });
   
