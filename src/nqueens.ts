fffddfd8ef6d72aa29e87021feabd54d39638f1c
Serhat1304 <serhatkus01@gmail.com>
2025-02-12 15:43:42 +0100

    Implémentation des fonctions pour trouver les solutions et validation n=4

--- src/nqueens.ts
+++ src/nqueens.ts
@@ -1,22 +1,48 @@
-export function solveNQueens(n: number) : string[][]{
+export function solveNQueens(n: number): string[][] {
     if (n === 1) {
         return [["#"]];
     }
-    if (n == 2 || n == 3) {
+    if (n === 2 || n === 3) {
         return [];
     }
-    return [];
-
-    const solutions : string [][] = [];
-    const cols = Array(n).fill(-1);
-
+  
+    const solutions: string[][] = [];
+    const cols = new Array(n).fill(-1);
     function searchQueens(row: number) {
-        if (row == n) {
-            return;
-        } for (let col = 0; col < n; col++) {
-            
+      if (row === n) {
+        solutions.push(buildBoard(cols));
+        return;
+      }
+  
+      for (let col = 0; col < n; col++) {
+        if (isValid(row, col)) {
+          cols[row] = col;
+          searchQueens(row + 1);
         }
+      }
     }
-
-    return solutions
-}
+  
+    function isValid(rowToPlace: number, colToPlace: number): boolean {
+      for (let r = 0; r < rowToPlace; r++) {
+        const c = cols[r];
+        if (c === colToPlace) return false;
+        if (Math.abs(r - rowToPlace) === Math.abs(c - colToPlace)) return false;
+      }
+      return true;
+    }
+  
+    function buildBoard(cols: number[]): string[] {
+      const board: string[] = [];
+      for (let r = 0; r < n; r++) {
+        let rowStr = "";
+        for (let c = 0; c < n; c++) {
+          rowStr += (cols[r] === c) ? "#" : "O";
+        }
+        board.push(rowStr);
+      }
+      return board;
+    }
+    searchQueens(0);
+  
+    return solutions;
+  }
